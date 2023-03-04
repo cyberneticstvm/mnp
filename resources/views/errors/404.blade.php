99d2d5f9d6e859eeffb721dc09b21a2c338b5d85
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-04 12:26:41 +0530

    small updates

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,17 @@
-@extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Custom Error Page Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5 pt-5">
+        <div class="alert alert-danger text-center">
+            <h2 class="display-3">404</h2>
+            <p class="display-5">The resources which you are looking is does not exist.</p>
+        </div>
+    </div>
+</body>
+</html>
